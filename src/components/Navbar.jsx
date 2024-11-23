352647d062630db95abc5a00a3c5ca65afd4edda
Shehorz Saleem <shehrozsaleem616@gmail.com>
2024-11-23 18:01:39 +0500

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import Link from 'next/link'
 import { buttonVariants } from './ui/button'
-
-const Navbar = () => {
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+const Navbar = async () => {
+    const session = await auth()
+      console.log(session)
     return (
         <nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
             <div className='container px-2.5 md:px-20'>
@@ -10,24 +13,29 @@ const Navbar = () => {
                         CreateA<span className='text-green-600'> Button</span>
                     </Link>
                     <div className='h-full flex items-center space-x-4'>
+                        { !session?.user && 
+                            <Link
+                                href='/signup'
+                                className={buttonVariants({
+                                    size: 'sm',
+                                    variant: 'ghost',
+                                })}>
+                                Sign up
+                            </Link>
+                        }
+                        { !session?.user && 
+                            <Link
+                                href='/signin'
+                                className={buttonVariants({
+                                    size: 'sm',
+                                    variant: 'ghost',
+                                })}>
+                                Login
+                            </Link>
+                        }
+                        
 
-                        <Link
-                            href='/signup'
-                            className={buttonVariants({
-                                size: 'sm',
-                                variant: 'ghost',
-                            })}>
-                            Sign up
-                        </Link>
-
-                        <Link
-                            href='/signin'
-                            className={buttonVariants({
-                                size: 'sm',
-                                variant: 'ghost',
-                            })}>
-                            Login
-                        </Link>
+                        
 
                         <div className='h-8 w-px bg-zinc-200 hidden sm:block' />
 
@@ -39,14 +47,17 @@ const Navbar = () => {
                             })}>
                             Create
                         </Link>
-                        <Link
-                            href='/dashboard'
-                            className={buttonVariants({
-                                size: 'sm',
-                                className: 'hidden sm:flex items-center gap-1',
-                            })}>
-                            Dashboard
-                        </Link>
+                        {session?.user &&  
+                            <Link
+                                href='/dashboard'
+                                className={buttonVariants({
+                                    size: 'sm',
+                                    className: 'hidden sm:flex items-center gap-1',
+                                })}>
+                                Dashboard
+                            </Link>
+                        }
+                        
 
                     </div>
                 </div>
--- src/hook/page.js
+++ src/hook/page.js
@@ -2,8 +2,7 @@
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 const Protected = async ({children}) => {
-  const session = await auth()
-  console.log(session)
+  
   return (
     session?.user ? children : redirect('/signin')  
   )
